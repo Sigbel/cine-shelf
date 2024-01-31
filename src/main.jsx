349c2d9cd0fd3e2ce42c39dd83ac0f1c529ed719
Henrique <hb.belgamo@gmail.com>
2024-01-30 23:07:33 -0300

    feat: configure the initial project files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// CSS
 import './index.css'
+
+// Pages
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Movie from './pages/Movie.jsx'
+import Search from './pages/Search.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App></App>}>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='movie/:id' element={<Movie></Movie>}></Route>
+          <Route path='search' element={<Search></Search>}></Route>
+        </Route>
+      </Routes>    
+    </BrowserRouter>
   </React.StrictMode>,
 )
