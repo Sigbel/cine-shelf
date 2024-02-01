0ffba5fd78bd20b00b0bb139caf7bbc31bfc3e8d
Henrique <hb.belgamo@gmail.com>
2024-01-31 22:03:36 -0300

    feat: add search component to get specific movies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,40 @@
-import { Link } from "react-router-dom";
-import {BiCameraMovie, BiSearchAlt2} from "react-icons/bi"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 // CSS
-import "./Navbar.css"
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  };
+
   return (
     <nav id="navbar">
       <h2>
-        <Link to="/"><BiCameraMovie />CineShelf</Link>
+        <Link to="/">
+          <BiCameraMovie />
+          CineShelf
+        </Link>
       </h2>
-      <form>
-        <input type="text" placeholder="Busque um filme"/>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
         <button type="submit">
-            <BiSearchAlt2></BiSearchAlt2>
+          <BiSearchAlt2></BiSearchAlt2>
         </button>
       </form>
     </nav>
